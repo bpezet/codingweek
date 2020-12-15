7efccd5e20bab8a3d1822ca46c0616290fe3ff2c
Benjamin Pezet <benjamin.pezet@telecomnancy.eu>
2020-12-15 15:10:47 +0100

    Changement de vue étape1 : Création parcours et Annuler

--- src/TN_Hiking/Main.java
+++ src/TN_Hiking/Main.java
@@ -13,6 +13,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
+        /**
         GestionnaireParcours gParcours = new GestionnaireParcours();
 
         primaryStage.setTitle("Parcours");
@@ -23,5 +24,20 @@ public class Main extends Application {
         Parent root = loader.load();
         primaryStage.setScene(new Scene(root, 800,800));
         primaryStage.show();
+         */
+        GestionnaireParcours gParcours = new GestionnaireParcours();
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("View/welcomeView.fxml"));
+        loader.setControllerFactory(iC->new WelcomeView(gParcours));
+        Parent root = loader.load();
+
+        primaryStage.setTitle("TN_Hiking");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
