afaabb9818a3c1618d092a74a5cb8c3cdad62fe6
Mohammed krimi <mohammed.krimi@telecomnancy.eu>
2020-12-15 16:52:21 +0100

    Barre menu dans le createur des etapes

--- src/TN_Hiking/View/ParcoursTarcerView.java
+++ src/TN_Hiking/View/ParcoursTarcerView.java
@@ -8,6 +8,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.MenuBar;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -17,7 +18,8 @@ import java.util.Observer;
 
 public class ParcoursTarcerView {
     @FXML
-    Pane mainPane;
+    MenuBar my_bar;
+
 
     GestionnaireParcours gParcours;
 
@@ -25,13 +27,18 @@ public class ParcoursTarcerView {
         this.gParcours = gParcours;
     }
 
-    public void finirParcours() {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Confirmation");
-        alert.setHeaderText("Félicitations");
-        alert.setContentText("Votre parcours est crée avec succès!");
+    public void changeSceneCreerParcours(javafx.event.ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("createParcoursView.fxml"));
+        loader.setControllerFactory(iC->new CreateParcoursView(this.gParcours));
+        Parent createParcoursParent = loader.load();
 
-        alert.showAndWait();
+        Scene createParcoursScene = new Scene(createParcoursParent);
+
+        Stage window = (Stage) my_bar.getScene().getWindow();
+
+        window.setScene(createParcoursScene);
+        window.show();
     }
     public void changeSceneTracerToWelcomeView(ActionEvent actionEvent) throws IOException {
         this.gParcours.deleteParcours();
@@ -49,4 +56,23 @@ public class ParcoursTarcerView {
     }
 
 
+    public void finirParcours(ActionEvent actionEvent) throws IOException {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Confirmation");
+        alert.setHeaderText("Félicitations");
+        alert.setContentText("Votre parcours est crée avec succès!");
+
+        alert.showAndWait();
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("welcomeView.fxml"));
+        loader.setControllerFactory(iC->new WelcomeView(this.gParcours));
+        Parent createWelcomeParent = loader.load();
+
+        Scene createWelcomeScene = new Scene(createWelcomeParent);
+
+        Stage window = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+
+        window.setScene(createWelcomeScene);
+        window.show();
+    }
 }
