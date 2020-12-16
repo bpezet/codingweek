2d25807c0000fc4ed6dba18284465dd46277b7bc
Romain Schaller <romain.schaller@telecomnancy.eu>
2020-12-16 01:05:03 +0100

    adding debugg tools + reshape menu

--- src/TN_Hiking/View/WelcomeView.java
+++ src/TN_Hiking/View/WelcomeView.java
@@ -1,12 +1,15 @@
 package TN_Hiking.View;
 
 import TN_Hiking.Gestionnaires.GestionnaireParcours;
+import TN_Hiking.Models.Etape;
+import TN_Hiking.Models.Parcours;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -15,6 +18,22 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class WelcomeView implements Initializable {
+    // FILE
+    @FXML
+    public MenuItem SaveButton;
+    @FXML
+    public MenuItem SaveAsButton;
+    @FXML
+    public MenuItem OpenFromButton;
+    @FXML
+    public MenuItem addGpxFromButton;
+    @FXML
+    public MenuItem RefreshButton;
+    //DEBUGG
+    @FXML
+    public MenuItem printActiveButton;
+    @FXML
+    public MenuItem printLocalButton;
 
     @FXML
     protected BorderPane mainPane;
@@ -74,12 +93,70 @@ public class WelcomeView implements Initializable {
         Pane view = object.getPane("showParcoursView",this.gestionnaireParcours);
         mainPane.setCenter(view);
     }
+    //#####################################
+    //######### FILE ######################
+    //#####################################
+    @FXML
+    public void setRefreshButton(){}
+    @FXML
+    public void setSaveButton() {}
+    @FXML
+    public void setSaveAsButton(){}
+    @FXML
+    public void setOpenFromButton(){}
+    @FXML
+    public void setAddGpxFromButton(){}
+
+    //#####################################
+    //######### FILE ######################
+    //#####################################
+    @FXML
+    public void setPrintActiveButton(){
+        // permet d'afficher le gestionnaire courant du array list
+        // pour l'exemple on se créer un gestionnaire
+        String name1 = "name1";
+        String name2 = "name2";
+        double lat1 = 49.008764;
+        double lat2 = 53.0097;
+        double long1 = 123.092;
+        double long2 = 450.302;
+
+        Etape etape1 = new Etape(name1,lat1,long1);
+        Etape etape2 = new Etape(name2,lat2,long2);
+
+        Parcours parcours = new Parcours("",0,"");
+        parcours.setDifficulte(3);
+        parcours.setDepart("Strasbourg");
+        parcours.setName("Mon Premier Parcours");
+        parcours.setEtapeDebut(etape1);
+        parcours.setEtapeFin(etape2);
+
+        Parcours parcours2 = new Parcours("",2,"");
+        parcours2.setDepart("Nancy");
+        parcours2.setDifficulte(4);
+        parcours2.setName("Mon Deuxieme Parcours");
+        parcours2.setEtapeDebut(etape2);
+        parcours2.setEtapeFin(etape1);
+
+        gestionnaireParcours.addParcours(parcours);
+        gestionnaireParcours.addParcours(parcours2);
+
+        gestionnaireParcours.showGestionnaire();
+
+    }
+    @FXML
+    public void setPrintLocalButton(){}
+
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
+
+
+
+
 
 
 }
