1793fc7a6dd457c170c672e95c0519950ead9e67
Romain Schaller <romain.schaller@telecomnancy.eu>
2020-12-16 11:10:30 +0100

    addGPX tool

--- src/TN_Hiking/View/WelcomeView.java
+++ src/TN_Hiking/View/WelcomeView.java
@@ -24,10 +24,16 @@ import javafx.scene.image.ImageView;
 
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.DirectoryIteratorException;
 import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -50,6 +56,10 @@ public class WelcomeView implements Initializable {
     public MenuItem printActiveButton;
     @FXML
     public MenuItem printLocalButton;
+
+    // First Main pane
+    @FXML
+    public Pane firstPane;
 
     @FXML
     protected BorderPane mainPane;
@@ -152,16 +162,72 @@ public class WelcomeView implements Initializable {
     FileChooser fileChooser;
     @FXML
     public void setSaveAsButton() {
+        // le path du fileChooser
+        String pathname = "";
         // le truc est la gestion du
         fileChooser = new FileChooser();
         //question: comment avoir acces a la window precedente
         //fileChooser.showOpenDialog();
+
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        Stage stage = (Stage) firstPane.getScene().getWindow();
+        File file = directoryChooser.showDialog(stage);
+
         // permet d'ouvrir le fichier
+
+        System.out.println(file.getPath().toString());
+
+        file = fileChooser.showOpenDialog(stage);
+        System.out.println(file.getPath().toString());
+
     }
     @FXML
     public void setOpenFromButton(){}
     @FXML
-    public void setAddGpxFromButton(){}
+    public void setAddGpxFromButton(){
+        // aim : add a new parcours made of only those point from .csv file
+        // we can only get coords for each etape
+        // not photo/name ect so we are going to name it like parcours[nomFile]
+        // etape1,etape2,etape3 ect..
+
+        Parcours NewParcours = new Parcours("NewParcours",0,"Tera");
+        int nbEtape = 0;
+        //FIRST STEP: find file
+        fileChooser = new FileChooser();
+        Stage stage = (Stage) firstPane.getScene().getWindow();
+        File file = fileChooser.showOpenDialog(stage);
+        System.out.println("Le fichier choisis est:"+file.getPath().toString());
+        //System.out.println(file.getName().toString());
+        NewParcours.setName("ExportParcours_"+file.getName().toString());
+        // Second Step: check if we can have access to it
+        BufferedReader bufferedReader = null; // for file reading
+        try {
+            bufferedReader = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                //Thirs step: write in the new parcors
+                System.out.println(line);
+                String[] split = line.split(";");
+                //System.out.println(split.length+"");
+                // for(int i = 0;i<split.length;i++) { System.out.println(split[i]); }
+                if (split.length == 2) {
+                    Etape etape = new Etape("Etape"+nbEtape,Double.parseDouble(split[0]),Double.parseDouble(split[1]));
+                    NewParcours.addEtape(etape);
+                } else {
+                    System.out.println("Error au splitage ligne: FILE NOT CONVENTIONAL");
+                }
+                nbEtape++;
+
+            }/*whileBalise*/ } catch(IOException e) { e.printStackTrace(); }
+        try {
+            assert bufferedReader != null;
+            bufferedReader.close();
+        } catch (IOException e){  e.printStackTrace(); }
+        //4° etape on ajoute le parcours au gestionnaire
+        gestionnaireParcours.addParcours(NewParcours);
+
+
+    }
 
     //#####################################
     //######### FILE ######################
