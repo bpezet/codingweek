c44f4f72b8045e063e771dcac3fff2551dd6863f
Mohammed krimi <mohammed.krimi@telecomnancy.eu>
2020-12-16 10:19:13 +0100

    Desactivation du choix creer parcours lors d'une creation de parcours

--- src/TN_Hiking/View/ParcoursViewEtape.java
+++ src/TN_Hiking/View/ParcoursViewEtape.java
@@ -6,17 +6,21 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class ParcoursViewEtape {
+public class ParcoursViewEtape implements Initializable {
 
     private GestionnaireParcours gestionnaireParcours;
     @FXML
@@ -27,6 +31,8 @@ public class ParcoursViewEtape {
     private TextField latitude;
     @FXML
     private TextField longitude;
+    @FXML
+    private MenuItem borderPane;
 
     /** Constructeur */
     public ParcoursViewEtape(GestionnaireParcours gestionnaireParcours){
@@ -79,5 +85,10 @@ public class ParcoursViewEtape {
     /** Bouton : fermer l'application*/
     public void closeApp() {
         Platform.exit();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        this.borderPane.setDisable(true);
     }
 }
