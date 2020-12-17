de99034caa6a74a8244a482e17dd782acd1da936
Romain Schaller <romain.schaller@telecomnancy.eu>
2020-12-17 01:47:28 +0100

    introduce JunitTest

---
+++ src/TN_Hiking/BD/JunitTest.java
@@ -0,0 +1,19 @@
+package TN_Hiking.BD;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class JunitTest {
+
+    @Test
+    public void testCapacitor() {
+
+        String abc = "abc";
+        assertEquals(abc,"abc");
+
+        }
+    }
+
+
+
